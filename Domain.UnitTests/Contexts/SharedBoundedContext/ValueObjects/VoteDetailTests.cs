cef552e6bd8720ed0fcbc09752e50575596d40b6
Hernan Alvarez <halvarez@Austranet.com>
2021-12-06 01:45:01 -0300

    Se mejoro la implementacion de lapropiedad DeltaOfVotes

---
+++ Domain.UnitTests/Contexts/SharedBoundedContext/ValueObjects/VoteDetailTests.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Contexts.SharedBoundedContext.ValueObjects;
+using FluentAssertions;
+using Xunit;
+
+namespace Domain.UnitTests.Contexts.SharedBoundedContext.ValueObjects
+{
+    public class VoteDetailTests
+    {
+        [Fact]
+        public void DeltaOfVotes_Success_Should_ReturnDeltaOfVotes()
+        {
+            const int upVotes = 4;
+            const int downVotes = 3;
+            const int expDelta = 1;
+
+            var voteDetail = new VoteDetail(upVotes, downVotes);
+
+            voteDetail.DeltaOfVotes.Should().Be(expDelta);
+        }
+
+        [Fact]
+        public void ValueEquals_Success_Should_ReturnTrue()
+        {
+            const int upVotesDetail1 = 4;
+            const int downVotesDetail1 = 3;
+
+            var voteDetail1 = new VoteDetail(upVotesDetail1, downVotesDetail1);
+            var voteDetail2 = new VoteDetail(upVotesDetail1, downVotesDetail1);
+
+            voteDetail1.ValueEquals(voteDetail2).Should().BeTrue();
+        }
+
+        [Fact]
+        public void ValueEquals_Success_Should_ReturnFalse()
+        {
+            const int upVotesDetail1 = 4;
+            const int downVotesDetail1 = 3;
+            const int upVotesDetail2 = 3;
+            const int downVotesDetail2 = 2;
+
+            var voteDetail1 = new VoteDetail(upVotesDetail1, downVotesDetail1);
+            var voteDetail2 = new VoteDetail(upVotesDetail2, downVotesDetail2);
+
+            voteDetail1.ValueEquals(voteDetail2).Should().BeFalse();
+        }
+    }
+}
---
+++ Domain/Contexts/SharedBoundedContext/ValueObjects/VoteDetail.cs
@@ -0,0 +1,33 @@
+﻿using Quicker.Domain.Abstracts;
+using System.Collections.Generic;
+
+namespace Domain.Contexts.SharedBoundedContext.ValueObjects
+{
+    public class VoteDetail : ValueObject
+    {
+        private VoteDetail() { }
+
+        public VoteDetail(int upVotes, int downVotes) : this()
+        {
+            UpVotes = upVotes;
+            DownVotes = downVotes;
+        }
+
+        protected override IEnumerable<object?> GetAtomicValues()
+        {
+            yield return UpVotes;
+            yield return DownVotes;
+            yield return DeltaOfVotes;
+        }
+
+        public int DeltaOfVotes
+        {
+            get => UpVotes - DownVotes;
+            private set { }
+        }
+
+        public int UpVotes { get; private set; }
+
+        public int DownVotes { get; private set; }
+    }
+}
