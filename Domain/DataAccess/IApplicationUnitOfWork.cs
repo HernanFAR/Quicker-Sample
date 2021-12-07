59b54dc609bbbe8f68d45cb7929fa9cfc63ba3e4
Hernan Alvarez <halvarez@Austranet.com>
2021-12-07 06:20:54 -0300

    Se agrego el UnifOfWork del Dominio

---
+++ Domain/DataAccess/IApplicationUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Contexts.AnswerBoundedContext.Core.AnswerAggregateRoot;
+using Domain.Contexts.QuestionBoundedContext.Core.QuestionAggregateRoot;
+using Quicker.Domain.DataAccess;
+using Quicker.Domain.DataAccess.Repositories;
+using System;
+
+namespace Domain.DataAccess
+{
+    public interface IApplicationUnitOfWork : IUnitOfWork
+    {
+        public IRepositoryAsync<Question, Guid> Questions { get; }
+
+        public IRepositoryAsync<Answer, Guid> Answers { get; }
+    }
+}
