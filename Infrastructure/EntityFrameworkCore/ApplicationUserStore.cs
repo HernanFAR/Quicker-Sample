b204b0bdb698f03abd3c93eb8a5aa1bc4d890752
Hernan Alvarez <halvarez@Austranet.com>
2021-12-12 13:49:32 -0300

    Se agrego un UserStore que emplea ApplicationUsers

---
+++ Infrastructure/EntityFrameworkCore/ApplicationUserStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Domain.Contexts.UserBoundedContext.Core;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace Infrastructure.EntityFrameworkCore
+{
+    public class ApplicationUserStore : UserStore<ApplicationUser, ApplicationRole, ApplicationDbContext, Guid>
+    {
+        public ApplicationUserStore(ApplicationDbContext context, IdentityErrorDescriber? describer = null) : 
+            base(context, describer) { }
+    }
+}
