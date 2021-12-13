05e879abe2cd3ffd205ec40382a0c2405d25c0da
Hernan Alvarez <halvarez@Austranet.com>
2021-12-12 23:18:20 -0300

    Se creo una interface para tener los datos minimos que debe mostrar una función query de Usuarios

---
+++ Application/Contexts/UserBoundedContext/CQRS/Users/Interfaces/IUserDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Application.Contexts.UserBoundedContext.CQRS.Users.Interfaces
+{
+    public interface IUserDetail
+    {
+        public Guid Id { get; }
+
+        public string Name { get; }
+
+        public string? SubName { get; }
+
+        public string UserName { get; }
+
+        public string Email { get; }
+
+        public string? PhoneNumber { get; }
+    }
+}
