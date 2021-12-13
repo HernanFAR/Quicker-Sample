2f431c639c10430f1e8b20f41e4b355b3a13f28c
Hernan Alvarez <halvarez@Austranet.com>
2021-12-12 23:27:23 -0300

    Se agrego la clase de configuration de JWT

---
+++ Domain/Contexts/UserBoundedContext/Configurations/JWTConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Contexts.UserBoundedContext.Configurations
+{
+    public class JWTConfiguration
+    {
+        public string IssuerSigningKey { get; set; } = string.Empty;
+
+        public string Issuer { get; set; } = string.Empty;
+
+        public string Audience { get; set; } = string.Empty;
+
+        public double Duration { get; set; }
+    }
+}
