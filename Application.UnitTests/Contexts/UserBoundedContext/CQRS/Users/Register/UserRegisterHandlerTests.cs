fc950e40a9ea734399fb801ed77479b3c500a38e
Hernan Alvarez <halvarez@Austranet.com>
2021-12-12 23:28:14 -0300

    Se agregaron los test de registrar usuarios

---
+++ Application.UnitTests/Contexts/UserBoundedContext/CQRS/Users/Register/UserRegisterHandlerTests.cs
@@ -0,0 +1,90 @@
+﻿using Application.Contexts.UserBoundedContext.CQRS.Users.Commands.Register;
+using Domain.Contexts.UserBoundedContext.Configurations;
+using Domain.Contexts.UserBoundedContext.Core;
+using FluentAssertions;
+using Infrastructure.EntityFrameworkCore;
+using Infrastructure.EntityFrameworkCore.RoleRelated;
+using Infrastructure.EntityFrameworkCore.UserRelated;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using SharedTestResources.Extensions;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Application.UnitTests.Contexts.UserBoundedContext.CQRS.Users.Register
+{
+    public class UserRegisterHandlerTests : IDisposable
+    {
+        private readonly IServiceProvider _ServiceProvider;
+
+        public UserRegisterHandlerTests()
+        {
+            _ServiceProvider = new ServiceCollection()
+                .AddLogging()
+                .AddScoped(_ => ContextManager.CreateContext(Guid.NewGuid()))
+                .AddEntityFrameworkSqlite()
+                .AddIdentity<ApplicationUser, ApplicationRole>()
+                .AddUserStore<ApplicationUserStore>()
+                .AddRoleStore<ApplicationRoleStore>()
+                .AddUserManager<ApplicationUserManager>()
+                .AddRoleManager<ApplicationRoleManager>()
+                .AddSignInManager<ApplicationSignInManager>()
+                .AddDefaultTokenProviders()
+                .Services
+                .Configure(IdentityConfiguration.ApplyIdentityOptionsConfiguration)
+                .BuildServiceProvider();
+        }
+
+        public void Dispose()
+        {
+            _ServiceProvider.GetRequiredService<ApplicationDbContext>().Dispose();
+        }
+
+        [Fact]
+        public async Task Handle_Success_Should_CreateUserWithInformation()
+        {
+            const int expCount = 2;
+
+            const string name = "Name";
+            const string userName = "userName";
+            const string email = "email@email.com";
+            const string phoneNumber = "+569 4979 8355";
+            const string subName = "subName";
+            const string password = "H5sdb.1234";
+
+            var command = new UserRegisterCommand
+            {
+                Name = name,
+                UserName = userName,
+                Email = email,
+                PhoneNumber = phoneNumber,
+                SubName = subName,
+                Password = password
+            };
+
+            var jwtConfiguration = new JWTConfiguration("XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD",
+                "XDDDDDDDDDDDDD", "XDDDDDDDDDDDDD", 2.5);
+
+            var handler = new UserRegisterHandler(_ServiceProvider.GetRequiredService<ApplicationSignInManager>(), jwtConfiguration);
+
+            var dto = await handler.Handle(command, default);
+
+            dto.Id.Should().NotBe(default(Guid));
+            dto.Name.Should().Be(name);
+            dto.UserName.Should().Be(userName);
+            dto.Email.Should().Be(email);
+            dto.PhoneNumber.Should().Be(phoneNumber);
+            dto.SubName.Should().Be(subName);
+
+            dto.Token.Should().NotBeNullOrEmpty();
+            dto.ExpireDate.Should().BeCloseTo(DateTime.Now.AddDays(jwtConfiguration.Duration), TimeSpan.FromHours(6));
+
+            var context = _ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+            context.Users.Count().Should().Be(expCount);
+            context.Users.Any(e => e.Id == dto.Id).Should().BeTrue();
+        }
+    }
+}
--- Application/Contexts/UserBoundedContext/CQRS/Users/Commands/Register/UserRegisterHandler.cs
+++ Application/Contexts/UserBoundedContext/CQRS/Users/Commands/Register/UserRegisterHandler.cs
@@ -1,15 +1,78 @@
-﻿using MediatR;
+﻿using Domain.Contexts.UserBoundedContext.Builders;
+using Domain.Contexts.UserBoundedContext.Configurations;
+using FluentValidation;
+using FluentValidation.Results;
+using Infrastructure.EntityFrameworkCore;
+using MediatR;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace Application.Contexts.UserBoundedContext.CQRS.Users.Commands.Register
 {
     public class UserRegisterHandler : IRequestHandler<UserRegisterCommand, UserRegisterDTO>
     {
-        public Task<UserRegisterDTO> Handle(UserRegisterCommand request, CancellationToken cancellationToken)
+        private readonly ApplicationSignInManager _SignInManager;
+        private readonly JWTConfiguration _JWTConfiguration;
+
+        public UserRegisterHandler(ApplicationSignInManager signInManager, JWTConfiguration jwtConfiguration)
         {
-            throw new NotImplementedException();
+            _SignInManager = signInManager;
+            _JWTConfiguration = jwtConfiguration;
+        }
+
+        public async Task<UserRegisterDTO> Handle(UserRegisterCommand request, CancellationToken cancellationToken)
+        {
+            using var scope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled);
+
+            var applicationUser = ApplicationUserCreateBuilder.Create
+                .WithRandomId().WithEmail(request.Email)
+                .WithName(request.Name).WithUserName(request.UserName)
+                .Continue
+                .WithPhoneNumber(request.PhoneNumber).WithSubName(request.SubName)
+                .GetInstance;
+
+            var addedUserResult = await _SignInManager.UserManager.CreateAsync(applicationUser, request.Password);
+
+            if (!addedUserResult.Succeeded)
+            {
+                throw new ValidationException(
+                    addedUserResult.Errors.Select(e =>
+                        new ValidationFailure(e.Code, e.Description)
+                    )
+                );
+            }
+
+            var identity = await _SignInManager.CreateUserPrincipalAsync(applicationUser);
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_JWTConfiguration.IssuerSigningKey));
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var jwt = new JwtSecurityToken(
+                issuer: _JWTConfiguration.Issuer,
+                audience: _JWTConfiguration.Audience,
+                claims: identity.Claims,
+                expires: DateTime.Now.AddDays(_JWTConfiguration.Duration),
+                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+            );
+
+            scope.Complete();
+
+            return new UserRegisterDTO
+            {
+                Token = tokenHandler.WriteToken(jwt),
+                ExpireDate = jwt.ValidTo,
+                Email = applicationUser.Email,
+                Id = applicationUser.Id,
+                Name = applicationUser.Name,
+                PhoneNumber = applicationUser.PhoneNumber,
+                SubName = applicationUser.SubName,
+                UserName = applicationUser.UserName
+            };
         }
     }
 }
