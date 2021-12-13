f80f003c3f4aaecf2d6afd9053284374d0a18022
Hernan Alvarez <halvarez@Austranet.com>
2021-12-12 23:23:04 -0300

    Se agregaron campos para la representacion del token JWT

--- Application/Contexts/UserBoundedContext/CQRS/Users/Commands/Register/UserRegisterCommand.cs
+++ Application/Contexts/UserBoundedContext/CQRS/Users/Commands/Register/UserRegisterCommand.cs
@@ -1,21 +1,26 @@
 ﻿using Quicker.Infrastructure.MediatR.Request;
 using System;
+using Application.Contexts.UserBoundedContext.CQRS.Users.Interfaces;
 
 namespace Application.Contexts.UserBoundedContext.CQRS.Users.Commands.Register
 {
-    public class UserRegisterDTO
+    public class UserRegisterDTO : IUserDetail
     {
         public Guid Id { get; set; }
 
         public string Name { get; set; } = string.Empty;
 
-        public string SubName { get; set; } = string.Empty;
+        public string? SubName { get; set; }
 
         public string UserName { get; set; } = string.Empty;
 
         public string Email { get; set; } = string.Empty;
 
-        public string PhoneNumber { get; set; } = string.Empty;
+        public string? PhoneNumber { get; set; }
+
+        public string Token { get; set; } = string.Empty;
+
+        public DateTime ExpireDate { get; set; }
     }
 
     public class UserRegisterCommand : ICommand<UserRegisterDTO>
@@ -29,5 +34,7 @@ namespace Application.Contexts.UserBoundedContext.CQRS.Users.Commands.Register
         public string Email { get; set; } = string.Empty;
 
         public string PhoneNumber { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
     }
 }
